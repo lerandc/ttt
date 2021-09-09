edc27417ee7c66ae0a8275aa6c0e0302baed6261
Luis Rangel DaCosta <luisrd@berkeley.edu>
2021-09-09 03:52:10 -0700

    backup

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 
 setup(
-            name='ttt',
-                version='0.1',
-                    packages=find_packages(),
-                        description='tricks, tidbits, and things',
-                            url='https://github.com/lerandc/ttt',
-                                author='Luis Rangel DaCosta',
-                                    author_email='luisrd@berkeley.edu',
-                                        python_requires='>=3.7',
-                                        )
+    name='ttt',
+    version='0.1',
+    packages=find_packages(),
+    description='tricks, tidbits, and things',
+    url='https://github.com/lerandc/ttt',
+    author='Luis Rangel DaCosta',
+    author_email='luisrd@berkeley.edu',
+    python_requires='>=3.7',
+    )

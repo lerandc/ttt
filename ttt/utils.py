944ca021854292f55af0f4f4be83046109a27693
Luis Rangel DaCosta <luisrd@berkeley.edu>
2021-09-28 10:26:08 -0700

    backup

--- ttt/utils.py
+++ ttt/utils.py
@@ -1,4 +1,5 @@
 import itertools
+import pathlib
 
 def kv_pairs(key, vals):
     return [(key, val) for val in vals]
@@ -8,3 +9,21 @@ def make_param_dict(keys, vals):
     print(kvs)
     p_list = list(itertools.product(*kvs))
     return [{p[0]:p[1] for p in pp} for pp in p_list]
+
+def is_hidden(path):
+
+    for x in str(path).split("/"):
+        if x.startswith(".") and x != "..":
+            return True
+
+    return False
+
+def listfiles(folder, include_hidden=False):
+    # generator for files in subdirectory
+
+    if include_hidden:
+        out = [x for x in pathlib.Path(folder).glob("**/*")]
+        return out
+    else:
+        out = [x for x in pathlib.Path(folder).glob("**/*") if not is_hidden(x)]
+        return out
